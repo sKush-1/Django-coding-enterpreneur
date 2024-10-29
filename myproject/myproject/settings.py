b77bfd99d18426ad15d6c6c89d808779af1fccfa
skush-1 <kushsumit27@gmail.com>
2024-10-29 11:52:14 +0530

    turn debug off

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1"
+DEBUG = str(os.environ.get('DEBUG')) == "0"
 
 DJANGO_ALLOWED_HOSTS = ['localhost']
 if not DEBUG:
